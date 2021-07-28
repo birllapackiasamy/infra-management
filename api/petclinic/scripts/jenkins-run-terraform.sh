53c5ecebee6deeeff77eb9bdcac8898391f779cf
Birlla <mailtobirlla@gmail.com>
2021-07-28 16:10:02 +0530

    Update jenkins-run-terraform.sh

--- api/petclinic/scripts/jenkins-run-terraform.sh
+++ api/petclinic/scripts/jenkins-run-terraform.sh
@@ -9,4 +9,4 @@ sed -i 's/terraform-state-f9x9tcl0/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
 aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
 terraform init
-terraform apply -auto-approve -var APP_INSTANCE_COUNT=1 -target aws_instance.app-instance
+terraform apply -auto-approve -var APP_INSTANCE_COUNT=1 -target aws_instance.app-instance -var AWS_ACCESS_KEY=$AWS_ACCESS_KEY  -var AWS_SECRET_KEY=$AWS_SECRET_KEY
