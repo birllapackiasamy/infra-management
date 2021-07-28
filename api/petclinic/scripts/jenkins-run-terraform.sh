7570c3b3b9befad3afa3b2643623e551a64702fd
Birlla <mailtobirlla@gmail.com>
2021-07-28 15:48:20 +0530

    Update jenkins-run-terraform.sh

--- api/petclinic/scripts/jenkins-run-terraform.sh
+++ api/petclinic/scripts/jenkins-run-terraform.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -ex
 AWS_REGION="ap-south-1"
+pwd
 cd ../
+pwd
+cd ../
+pwd
 S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
 sed -i 's/terraform-state-f9x9tcl0/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
